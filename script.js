74a2a41f3fb22e0896dbcc91af87468f11987f48
RishieVyas <rishievyas@gmail.com>
2025-04-19 01:01:14 -0400

    Let's add update functionality for items

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ const itemInput = document.getElementById('item-input')
 const itemList = document.getElementById('item-list')
 const itemClear = document.getElementById('clear')
 const itemFilter = document.getElementById('filter')
+const submitFormButton = itemForm.querySelector('button')
+let isEditMode = false
 
-function addItem (e) {
+// Functions to add items to DOM and Local Storage: "CREATE"
+
+function onAddItemSubmit (e) {
     e.preventDefault();
     const newItem = itemInput.value
 
@@ -13,14 +17,37 @@ function addItem (e) {
         return;
     }
 
+    if(isEditMode){
+        const itemToEdit = itemList.querySelector('.edit-mode');
+        removeItemFromStorage(itemToEdit.textContent);
+        itemToEdit.classList.remove('edit-mode');
+        itemToEdit.remove();
+        isEditMode = false;
+    } else {
+        if(checkIfItemExist(newItem)){
+            console.log('already exist')
+            alert('Item already exist!');
+            return;
+        }
+    }
+
+    // Create the list item and add it to the DOM
+    addItemToDOM(newItem)
+
+    // Add Item to local storage 
+    addItemToStorage(newItem)
+
+    resetUI();
+    itemInput.value = ''
+}
+
+function addItemToDOM (newItem) {
     const li = document.createElement('li');
     li.appendChild(document.createTextNode(newItem));
 
     const button = createButton('remove-item btn-link text-red')
     li.appendChild(button)
     itemList.appendChild(li)
-    itemInput.value = ''
-    resetUI();
 }
 
 function createButton (className) {
@@ -32,13 +59,85 @@ function createButton (className) {
     return button;
 }
 
-function removeItem (e) {
-    if(e.target.parentElement.classList.contains('remove-item')){
-        if(confirm('Are you sure? You want to delete the item.')){
-            e.target.parentElement.parentElement.remove();
-            resetUI();
-        }
+function addItemToStorage (item) {
+    const itemsFromStorage = getItemsFromStorage();
+
+    // Add new item to storage
+    itemsFromStorage.push(item);
+
+    // Convert to JSON string and set to local storage
+    localStorage.setItem('items', JSON.stringify(itemsFromStorage))
+}
+
+
+// Getting stored items from local storage: "READ"
+
+function getItemsFromStorage () {
+    let itemsFromStorage;
+    if (localStorage.getItem('items') === null){
+        itemsFromStorage = []
+    } else {
+        itemsFromStorage = JSON.parse(localStorage.getItem('items'));
     }
+
+    return itemsFromStorage;
+}
+
+function displayItems () {
+    const itemsFromStorage = getItemsFromStorage();
+    itemsFromStorage.forEach(item => addItemToDOM(item))
+    resetUI();
+}
+
+
+function onClickItem (e) {
+    if(e.target.parentElement.classList.contains('remove-item')){
+        removeItem(e.target.parentElement.parentElement)
+    } else {
+        setItemToEdit(e.target)
+    }
+}
+
+function checkIfItemExist (item) {
+    const itemsFromStorage = getItemsFromStorage();
+    console.log(itemsFromStorage)
+    console.log(item)
+    return itemsFromStorage.includes(item);
+}
+
+
+// Edit the item: "Update"
+
+function setItemToEdit (item) {
+    isEditMode = true;
+    itemList.querySelectorAll('li').forEach((i) => i.classList.remove('edit-mode'))
+
+    item.classList.add('edit-mode');
+    submitFormButton.innerHTML = '<i class="fa-solid fa-pen"></i> Update Item';
+    submitFormButton.style.color = "white"
+    submitFormButton.style.backgroundColor = "#228B22"
+    itemInput.value = item.textContent.trim();
+}
+
+
+// Deleting items from storage and DOM: "DELETE"
+
+function removeItem (item) {
+    if(confirm('Are you sure? You want to delete the item.')){
+        item.remove();
+        removeItemFromStorage(item.textContent)
+        resetUI();
+    }
+}
+
+function removeItemFromStorage (item) {
+    let itemsFromStorage = getItemsFromStorage();
+
+    // Filtering out rest of the element apart from one that is deleted
+    itemsFromStorage = itemsFromStorage.filter(i => i !== item)
+
+    // Setting the filtered array to local storage
+    localStorage.setItem('items', JSON.stringify(itemsFromStorage))
 }
 
 function clearAllItems() {
@@ -47,6 +146,10 @@ function clearAllItems() {
             itemList.removeChild(itemList.firstChild)
         }
     }
+
+    // Clear from local storage
+    localStorage.removeItem('items')
+
     resetUI();
 }
 
@@ -66,6 +169,7 @@ function filterItems (e) {
 }
 
 function resetUI () {
+    itemInput.value = '';
     const item = document.querySelectorAll('li')
 
     if(item.length === 0){
@@ -76,10 +180,33 @@ function resetUI () {
         itemClear.style.display = 'block';
         itemFilter.style.display = 'block'
     }
+    submitFormButton.innerHTML = '<i class="fa-solid fa-plus" ></i> Add Item'
+    submitFormButton.style.backgroundColor = 'black'
+    submitFormButton.style.color = 'white'
+    isEditMode = false
 }
 
-// Event Listeners
-itemForm.addEventListener('submit', addItem);
-itemList.addEventListener('click', removeItem)
-itemClear.addEventListener('click', clearAllItems)
-itemFilter.addEventListener('input', filterItems)
+// Initialize App
+
+function init() {
+    // Event Listeners
+    itemForm.addEventListener('submit', onAddItemSubmit);
+    itemList.addEventListener('click', onClickItem)
+    itemClear.addEventListener('click', clearAllItems)
+    itemFilter.addEventListener('input', filterItems)
+    document.addEventListener('DOMContentLoaded', displayItems)
+    resetUI();
+}
+
+init();
+
+/*
+Local storage and session storage are property on Window interface and the data is
+stored locally on our browser. It gets stored in key/value pair and values are in
+strings. We can not store complete object, need to strigify them.
+*/
+
+// localStorage.setItem('name', 'Brad')
+// console.log(localStorage.getItem('name'))
+// localStorage.clear();
+// sessionStorage.clear();
